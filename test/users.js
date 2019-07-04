60c53053f1fa458f565b180ab5dfac8b1832cb8f
ucheka22 <ryanucheka@gmail.com>
2019-07-04 11:50:25 +0100

    chore(unit test):implement unit test(TDD) for API [Finishes #167089207]

--- test/users.js
+++ test/users.js
@@ -7,22 +7,25 @@ const { expect } = chai;
 chai.use(chaiHttp);
 
 describe('API TESTING', () => {
-	const user = {
-		token: '4535',
-		id: 1,
-		first_name: 'Chike',
-		last_name: 'Ucheka',
-		email: 'ryanucheka@gmail.com',
-		password: 'chike22ucheka'
-	};
-
 	describe('/POST/user/auth/signup', () => {
+		const user = {
+			token: '45erkjherht45495783',
+			id: 1,
+			email: 'johndoe@gmail.com',
+			first_name: 'John',
+			last_name: 'Doe',
+			password: 'john22doe',
+			phoneNumber: '09030467496',
+			address: 'No 14 Alaska Street',
+			is_admin: true
+		};
+
 		it('it should create a new user', (done) => {
 			chai
 				.request(app)
 				.post('/api/v1/user/auth/signup')
 				.set('Accept', 'application/json')
-				.send(JSON.stringify(user))
+				.send(user)
 				.end((err, res) => {
 					expect(res).to.have.status(201);
 					expect(res.body.data).to.include({
@@ -36,10 +39,36 @@ describe('API TESTING', () => {
 					done(err);
 				});
 		});
+		it('it should not create user if user exists', (done) => {
+			user.email = 'ryanucheka@gmail.com';
+			chai
+				.request(app)
+				.post('/api/v1/user/auth/signup')
+				.set('Accept', 'application/json')
+				.send(user)
+				.end((err, res) => {
+					expect(res).to.have.status(400);
+					expect(res.body.status).to.equal('error');
+					expect(res.body.error).to.equal('email already taken');
+					done(err);
+				});
+		});
 	});
 
 	describe('/POST/user/auth/signin', () => {
 		it('it should login user', (done) => {
+			const user = {
+				token: '45erkjherht45495783',
+				id: 1,
+				email: 'ryanucheka@gmail.com',
+				first_name: 'Chike',
+				last_name: 'Ucheka',
+				password: 'chike22ucheka',
+				phoneNumber: '09030467496',
+				address: 'No 14 Alfa Oyo street ',
+				is_admin: true
+			};
+
 			const payload = {
 				email: user.email,
 				password: user.password
@@ -48,7 +77,7 @@ describe('API TESTING', () => {
 				.request(app)
 				.post('/api/v1/user/auth/signin')
 				.set('Accept', 'application/json')
-				.send(JSON.stringify(payload))
+				.send(payload)
 				.end((err, res) => {
 					expect(res).to.have.status(201);
 					expect(res.body.data).to.include({
@@ -59,6 +88,23 @@ describe('API TESTING', () => {
 						email: user.email,
 						password: user.password
 					});
+					done(err);
+				});
+		});
+		it('it should not login user with incorrect credentials', (done) => {
+			const payload = {
+				email: 'invalid@gmail.com',
+				password: 'incorrectpassword'
+			};
+			chai
+				.request(app)
+				.post('/api/v1/user/auth/signin')
+				.set('Accept', 'application/json')
+				.send(payload)
+				.end((err, res) => {
+					expect(res).to.have.status(401);
+					expect(res.body.status).to.equal('error');
+					expect(res.body.error).to.equal('Invalid email or password!!');
 					done(err);
 				});
 		});
