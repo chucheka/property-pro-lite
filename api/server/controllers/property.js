2803c6181b6c7ea09c6cbe99d0df1be575e77ba2
ucheka22 <ryanucheka@gmail.com>
2019-07-09 19:57:44 +0100

    chore(heroku deploy) deploy ap to heroku [Finishes #167166856]

--- api/server/controllers/property.js
+++ api/server/controllers/property.js
@@ -12,9 +12,10 @@ cloudinary.config({
 });
 export default class PropertyController {
 	// FOR CREATING PROPERTY ADVERT
-	static async postAdvert(req, res) {
+	static postAdvert(req, res) {
 		const newAD = req.body;
 		const noFile = req.file;
+		console.log(noFile);
 		if (!noFile) {
 			const oldAD = properties.find((ad) => ad.id === newAD.id);
 			if (oldAD) {
@@ -24,47 +25,52 @@ export default class PropertyController {
 				});
 			}
 			properties.push(newAD);
-			return res.status(201).json({
-				status: 'success',
-				data: properties[properties.length - 1]
-			});
-		}
-		try {
-			const result = await cloudinary.v2.uploader.upload(req.file.path, {
-				public_id: `imageUploads/${req.file.originalname}`,
-				use_filename: true,
-				unique_filename: false
-			});
-			console.log(result);
-			newAD.image_url = result.public_id;
-			properties.push(newAD);
 			res.status(201).json({
 				status: 'success',
 				data: properties[properties.length - 1]
 			});
-		} catch (err) {
-			console.log('Image upload failed');
-			console.log(err);
-			res.status(400).json({
-				status: 'error'
-				// error: 'Image upload unsuccessful'
-			});
 		}
+
+		// return cloudost(req, res){
+		// 		try {
+		// 			const result = await cloudinary.v2.uploader.upload(req.file.path, {
+		// 				public_id: `imageUploads/${req.file.originalname}`,
+		// 				use_filename: true,
+		// 				unique_filename: false
+		// 			})
+		// 			console.log(result);
+		// 			newAD.image_url = result.public_id;
+		// 			properties.push(newAD);
+		// 			res.status(201).json({
+		// 				status: 'success',
+		// 				data: properties[properties.length - 1]
+		// 			})
+		// 		} catch (err) {
+		// 			console.log('Image upload failed');
+		// 			console.log(err);
+		// 			res.status(400).json({
+		// 				status: 'error'
+		// 				// error: 'Image upload unsuccessful'
+		// 			})
+		// 		}
+		// 	}
+
+		// }
 	}
 
 	static async deleteAdvert(req, res) {
-		const Id = req.params.propertyId;
-		const arrLen = properties.length;
+		const Id = parseInt(req.params.propertyId, 10);
+		// const arrLen = properties.length;
 		const deleteIndex = properties.findIndex((property) => property.id === Id);
 		if (deleteIndex === -1) {
-			return res.status(404).json({
+			res.status(404).json({
 				status: 'error',
 				error: 'Property ad does not exist'
 			});
-		}
-		properties.splice(deleteIndex, 1);
-		const newLen = properties.length;
-		if (arrLen > newLen) {
+		} else {
+			properties.splice(deleteIndex, 1);
+			// const newLen = properties.length;
+
 			res.status(201).json({
 				status: 'success',
 				data: {
@@ -72,16 +78,17 @@ export default class PropertyController {
 				}
 			});
 		}
-		if (!req.file) {
-			try {
-				const imageId = properties[deleteIndex].image_url;
-				console.log(imageId);
-				const result = await cloudinary.v2.api.delete_resources([ imageId ]);
-				console.log(result);
-			} catch (err) {
-				console.log(err);
-			}
-		}
+
+		// if (!req.file) {
+		// 	try {
+		// 		const imageId = properties[deleteIndex].image_url;
+		// 		console.log(imageId);
+		// 		const result = await cloudinary.v2.api.delete_resources([ imageId ]);
+		// 		console.log(result);
+		// 	} catch (err) {
+		// 		console.log(err);
+		// 	}
+		// }
 	}
 
 	// TO MARK PROPERTY AS SOLD
@@ -107,7 +114,7 @@ export default class PropertyController {
 		const Id = parseInt(req.params.propertyId, 10);
 		const toUpdateAD = properties.find((property) => property.id === Id);
 		if (!toUpdateAD) {
-			res.status(400).json({
+			res.status(404).json({
 				status: 'error',
 				error: 'Advert does not exist'
 			});
@@ -131,7 +138,7 @@ export default class PropertyController {
 				status: 'success',
 				data: properties
 			});
-		} else {
+		} else if (len === 0) {
 			res.status(404).json({
 				status: 'error',
 				error: 'There are no property ads'
@@ -139,12 +146,12 @@ export default class PropertyController {
 		}
 	}
 
-	static async getSpecificProperty(req, res) {
+	static getSpecificProperty(req, res) {
 		const propType = req.query.type;
 		const props = [];
-		properties.forEach((element) => {
-			if (element.type === propType) {
-				props.push(element);
+		properties.forEach((prop) => {
+			if (prop.type === propType) {
+				props.push(prop);
 			}
 		});
 		if (props.length === 0) {
@@ -160,18 +167,19 @@ export default class PropertyController {
 		}
 	}
 
-	static async getPropertyADvert(req, res) {
+	static getPropertyADvert(req, res) {
 		const Id = parseInt(req.params.propertyId, 10);
+		console.log(Id);
 		const property = properties.find((prop) => prop.id === Id);
-		if (!property) {
-			res.status(404).json({
-				status: 'error',
-				error: 'Cannot find property ad'
-			});
-		} else {
+		if (property) {
 			res.status(200).json({
 				status: 'success',
 				data: property
+			});
+		} else {
+			res.status(404).json({
+				status: 'error',
+				error: 'Cannot find property ad'
 			});
 		}
 	}
